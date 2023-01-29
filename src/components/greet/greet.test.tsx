ddd3089d8e7ebfd95f4cb8e1e40b0fe6b1e57e63
Matt <mlee138@binghamton.edu>
2023-01-29 11:42:11 -0500

    add code coverage script

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,15 +1,16 @@
-import { render, screen } from "@testing-library/react"
-import { Greet } from "./greet"
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
 
+describe("Greet", () => {
+  test("hello rendered", () => {
+    render(<Greet />);
+    const text = screen.getByText(/hello/i);
+    expect(text).toBeInTheDocument();
+  });
 
-test("Greet hello rendered", () => {
-  render(<Greet/>)
-  const text = screen.getByText(/hello/i);
-  expect(text).toBeInTheDocument();
-})
-
-test("Greet renders with a name", () => {
-  render(<Greet name="Steve" />);
-  const textElement = screen.getByText("Hello Steve");
-  expect(textElement).toBeInTheDocument();
-})
+  test("renders a name", () => {
+    render(<Greet name="Steve" />);
+    const textElement = screen.getByText(/Steve/);
+    expect(textElement).toBeInTheDocument();
+  });
+});
