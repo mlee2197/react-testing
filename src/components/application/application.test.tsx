93436766a74cee7bfadf3c209208205643300ea7
Matthew Lee <mlee2197@gmail.com>
2023-01-30 19:49:49 -0500

    create application for RTL queries

---
+++ src/components/application/application.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { Application } from "./application";
+
+describe("application", () => {
+  test("text input rendered", () => {
+    render(<Application />);
+
+    const titleElement = screen.getByRole("heading", { level: 1 });
+    expect(titleElement).toBeInTheDocument();
+
+    const subtitleElement = screen.getByRole("heading", { level: 2 });
+    expect(subtitleElement).toBeInTheDocument();
+
+    const paraElement = screen.getByText("All fields are mandatory");
+    expect(paraElement).toBeInTheDocument();
+
+    const spanElement = screen.getByTitle("close");
+    expect(spanElement).toBeInTheDocument();
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+
+    const imgElement = screen.getByAltText("a person with a laptop");
+    expect(imgElement).toBeInTheDocument();
+
+    const textElement = screen.getByPlaceholderText("Fullname");
+    expect(textElement).toBeInTheDocument();
+
+    const bioElement = screen.getByLabelText("Bio");
+    expect(bioElement).toBeInTheDocument();
+
+    const jobElement = screen.getByRole("combobox");
+    expect(jobElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+
+    const submitButton = screen.getByRole("button");
+    expect(submitButton).toBeInTheDocument();
+  });
+});
